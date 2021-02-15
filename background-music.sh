cd3fe647625a72dedbd0e345f4c5b0d98a569ffe
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-02-15 03:50:01 -0400

    Autocommit por GitHub: alterações: add 'background-music.sh'

---
+++ background-music.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#
+# Arquivo: background-music.sh
+#
+# Feito por Lucas Saliés Brum a.k.a. sistematico, <lucas@archlinux.com.br>
+#
+# Criado em: 15/02/2021 03:46:17
+# Última alteração: 15/02/2021 03:46:12
